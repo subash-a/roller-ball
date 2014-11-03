c5b8a996476ee2dcbc2692fef87d3dd3941fbf5f
Subhash A <s.a@sap.com>
2014-11-03 09:18:13 +0530

    added inital commit of all files.

---
+++ Assets/Scripts/CameraController.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var gameSphere : GameObject;
+var offset : Vector3;
+
+function Start () {
+	offset = transform.position;
+}
+
+function LateUpdate () {
+	transform.position = gameSphere.transform.position + offset;
+}
---
+++ Assets/Scripts/SphereScript.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+var scoreCount = 0;
+var scoreText = new GUIText();
+var timeText = new GUIText();
+var winText = new GUIText();
+var scoreLimit = 4;
+var completeTime = 0;
+var currentTime = 0;
+
+function Start () {
+	scoreCount = 0;
+	winText.text = "";
+}
+
+function Update() {
+	currentTime = Time.time;
+	timeText.text = "Time: " + currentTime;
+}
+
+function FixedUpdate () {
+	var moveX = Input.GetAxis("Horizontal");
+	var moveZ = Input.GetAxis("Vertical");
+	var movement = new Vector3(moveX, 0.0, moveZ);
+	rigidbody.AddForce(movement);
+}
+
+function OnTriggerEnter(other: Collider) {
+	var gameObject = other.gameObject;
+	if(gameObject.CompareTag("pickUp")) {
+		gameObject.SetActive(false);
+		scoreCount = scoreCount + 1;
+		scoreText.text = "Score: " + scoreCount;
+		if(parseInt(scoreCount) == scoreLimit) {
+			winText.text = "You Finished";
+		}
+	}
+	
+}
